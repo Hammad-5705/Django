df59dffd5973c9544c01ed219f8833d0f38eb6d7
Muhammad Hammad Nadeem <hamadnad2014@gmail.com>
2025-02-19 21:14:46 +0500

    Completed Text Utils

--- django01/Task01/mysite/fx.py
+++ django01/Task01/mysite/fx.py
@@ -1,57 +1,54 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
-from django.shortcuts import render  
+ 
 
 def Fx(request): 
-    return render(request, "home.html")
+    return render(request, "boot.html")
 
 
-def analyze(request):
-    djtext= request.GET.get("txt","Text Didn\'t found")
-    final_result=djtext
-    rempunc= request.GET.get("rempunc","off")
-    Capatlize= request.GET.get("Capatlize","off")
-    Newline_remover=request.GET.get("Newline_remover","off")
-    Space_remover=request.GET.get("Space_remover","off")
-    Char_counter=request.GET.get("Char_counter","off")
+from django.http import HttpResponse  
+from django.shortcuts import render  
 
-    dict={"rempunc":rempunc,"Capatlize":Capatlize,"Newline_remover":Newline_remover,"Space_remover":Space_remover}
-    func={"rempunc":frempunc,"Capatlize":fCapatlize ,"Newline_remover":fNewline_remover,"Space_remover":fSpace_remover}
+def analyze(request):  
+    djtext = request.POST.get("txt", "Text Didn't found")  
+    final_result = djtext  
+    rempunc = request.POST.get("rempunc", "off")  
+    Capatlize = request.POST.get("Capatlize", "off")  
+    Newline_remover = request.POST.get("Newline_remover", "off")  
+    Space_remover = request.POST.get("Space_remover", "off")  
+    Char_counter = request.POST.get("Char_counter", "off")  
+
+    dct = {"rempunc": rempunc, "Capatlize": Capatlize, "Newline_remover": Newline_remover, "Space_remover": Space_remover}  
+    func = {"rempunc": frempunc, "Capatlize": fCapatlize, "Newline_remover": fNewline_remover, "Space_remover": fSpace_remover}  
+
+    raw_purpose = []  
+    cntr = 0  
+     
+    for i, j in dct.items():  
+        if j == "on":  
+            final_result = func[i](final_result)  
+            raw_purpose.append(i)  
+            cntr += 1  
     
-    cntr=0
-    raw_purpose=[]
-    for i,j in dict.items():
-        if j=="on":
-            final_result=func[i](final_result)
-            raw_purpose.append(i)
-            cntr+=1
-        
-    
-    
-    if cntr==0:
-        return HttpResponse("Error")
 
-    purpose=""
-    for i in raw_purpose:
-        purpose=f"{purpose}\n{str(i)}"
+    if cntr == 0 and not (Char_counter == "on"):  
+        data = {"Purpose": "Selected Nothing", "solved": f"404 Error"} 
+        return render(request, "analyze.html",data)  
 
-    if Char_counter=="on":
-        no=fChar_counter(final_result)
-        data = {"Purpose":purpose , "solved": f"{final_result}\nTotal Characters: {no}"} 
-    else:
-        data = {"Purpose":purpose , "solved": final_result} 
+    purpose = "\n".join(raw_purpose)
 
-    return render(request,"analyze.html",data)
+    if Char_counter == "on":  
+        no = fChar_counter(final_result)
+        purpose=f"{purpose} Count Characters"
+        data = {"Purpose": purpose, "solved": f"{final_result}", "no": f"Total Characters {no}"}  
+    else:  
+        data = {"Purpose": purpose, "solved": final_result}  
+
+    return render(request, "analyze.html", data)
             
 
-        
-
-
-
-
 def frempunc(djtext):
-    final_result=""
+    fnal_result=""
     punc = '''!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~'''  
     result = []  
 
@@ -59,48 +56,44 @@ def frempunc(djtext):
         if char not in punc:  
             result.append(char)  
         
-    final_result = ''.join(result)     
-    return str(final_result)
+    fnal_result = ''.join(result)     
+    return str(fnal_result)
     
 
 def fCapatlize(djtext):
-    final_result=""
+    fnal_result=""
     for char in djtext:
-        final_result= final_result + char.title()
+        fnal_result= fnal_result + char.title()
                 
 
 
-    return str(final_result)
+    return str(fnal_result)
         
 
-def fNewline_remover(djtext):
-    final_result=""
-    for index , char in enumerate(djtext):
-        if not(djtext[index]=="\n" and djtext[index+1]=="\n" and index<=len(djtext)):
-            final_result= final_result + char
-                    
-
-    return str(final_result)
+def fNewline_remover(djtext):    
+    lines = djtext.splitlines()  
+    result = [line for line in lines if line.strip()]
+    return "\n".join(result)
 
 
 
 
 def fSpace_remover(djtext):
-    final_result = ""  
+    fnal_result = ""  
 
     for index, char in enumerate(djtext):
         if (char == " " and index < len(djtext) - 1 and djtext[index + 1] == " "):  
             pass
         else:
-            final_result += char
+            fnal_result += char
 
-    return str(final_result)
+    return str(fnal_result)
 
 
 
 def fChar_counter(djtext):
-    final_result=len(djtext)
-    return f"{final_result}"
+    fnal_result=len(djtext)
+    return f"{fnal_result}"
 
 
 
@@ -113,78 +106,78 @@ def fChar_counter(djtext):
 
 
 
-def Analyze(request):
-    final_result=""
-    djtext= request.GET.get("txt","Text Didn\'t found")
-    rempunc= request.GET.get("rempunc","off")
-    Capatlize= request.GET.get("Capatlize","off")
-    Newline_remover=request.GET.get("Newline_remover","off")
-    Space_remover=request.GET.get("Space_remover","off")
-    Char_counter=request.GET.get("Char_counter","off")
+# def Analyze(request):
+#     final_result=""
+#     djtext= request.GET.get("txt","Text Didn\'t found")
+#     rempunc= request.GET.get("rempunc","off")
+#     Capatlize= request.GET.get("Capatlize","off")
+#     Newline_remover=request.GET.get("Newline_remover","off")
+#     Space_remover=request.GET.get("Space_remover","off")
+#     Char_counter=request.GET.get("Char_counter","off")
 
 
 
-    if rempunc=="on":
+#     if rempunc=="on":
     
-        punc = '''!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~'''  
-        result = []  
+#         punc = '''!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~'''  
+#         result = []  
 
-        for char in djtext:  
-            if char not in punc:  
-                result.append(char)  
+#         for char in djtext:  
+#             if char not in punc:  
+#                 result.append(char)  
         
-        final_result = ''.join(result)   
-        data = {"Purpose": "Remove Punctuations", "solved": final_result}  
+#         final_result = ''.join(result)   
+#         data = {"Purpose": "Remove Punctuations", "solved": final_result}  
 
 
-        return render(request,"analyze.html",data)
+#         return render(request,"analyze.html",data)
     
 
-    elif Capatlize=="on":
+#     elif Capatlize=="on":
 
-        for char in djtext:
-            final_result= final_result + char.title()
+#         for char in djtext:
+#             final_result= final_result + char.title()
         
-        data = {"Purpose": "Capatilize", "solved": final_result} 
-        return render(request,"analyze.html",data)
+#         data = {"Purpose": "Capatilize", "solved": final_result} 
+#         return render(request,"analyze.html",data)
     
 
-    elif Newline_remover=="on":
+#     elif Newline_remover=="on":
 
-        for index , char in enumerate(djtext):
-            if not(djtext[index]=="\n" and djtext[index+1]=="\n"):
-               final_result= final_result + char
+#         for index , char in enumerate(djtext):
+#             if not(djtext[index]=="\n" and djtext[index+1]=="\n"):
+#                final_result= final_result + char
                 
 
-        data = {"Purpose": "Remove Newlines", "solved": final_result} 
-        return render(request,"analyze.html",data)
+#         data = {"Purpose": "Remove Newlines", "solved": final_result} 
+#         return render(request,"analyze.html",data)
 
 
 
 
-    elif Space_remover=="on":
-        final_result = ""  
+#     elif Space_remover=="on":
+#         final_result = ""  
 
-        for index, char in enumerate(djtext):
-            if (char == " " and index < len(djtext) - 1 and djtext[index + 1] == " "):  
-                pass
-            else:
-                final_result += char
+#         for index, char in enumerate(djtext):
+#             if (char == " " and index < len(djtext) - 1 and djtext[index + 1] == " "):  
+#                 pass
+#             else:
+#                 final_result += char
 
-        data = {"Purpose": "Remove Extra Spaces", "solved": final_result} 
+#         data = {"Purpose": "Remove Extra Spaces", "solved": final_result} 
         
-        return render(request,"analyze.html",data)
+#         return render(request,"analyze.html",data)
 
 
 
-    elif Char_counter=="on":
-        final_result=len(djtext)
-        data = {"Purpose": "Count Characters", "solved": final_result} 
-        return render(request,"analyze.html",data)
+#     elif Char_counter=="on":
+#         final_result=len(djtext)
+#         data = {"Purpose": "Count Characters", "solved": final_result} 
+#         return render(request,"analyze.html",data)
 
 
-    else:
-        return HttpResponse("ERROR")
+#     else:
+#         return HttpResponse("ERROR")
 
 
 
