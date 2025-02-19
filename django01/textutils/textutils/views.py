638095e063841ef492d0cfc3d9050261f5303300
Muhammad Hammad Nadeem <hamadnad2014@gmail.com>
2025-02-19 21:16:44 +0500

    Text analyzing completed

--- django01/textutils/textutils/views.py
+++
@@ -1,9 +0,0 @@
-#Self created
-
-from django.http import HttpResponse
-
-def index(request):
-    return HttpResponse("<h1>Hello World<h1>")
-
-def ab(request):
-    return HttpResponse(f"about hello world".title())
